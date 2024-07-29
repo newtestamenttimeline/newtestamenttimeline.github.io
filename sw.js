f145409d3532a112e685d6ba237b86f409f84502
newtestamenttimeline <rognvaldur.gudmunds@gmail.com>
2024-07-29 17:03:49 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'timeline-cache-v1';
+const urlsToCache = [
+    '/',
+    '/styles.css',
+    '/scripts.js',
+    '/events.json',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
