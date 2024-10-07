14a7f4c8056979033612f2df211a8f9163c9ac05
newtestamenttimeline <rognvaldur.gudmunds@gmail.com>
2024-10-07 08:12:28 +0000

    Update uiControls.js

--- uiControls.js
+++ uiControls.js
@@ -31,7 +31,7 @@ function generateLegend() {
         questionMark.textContent = '?';
         const tooltip = document.createElement('span');
         tooltip.className = 'tooltip';
-        tooltip.textContent = `Filter events by ${eventType} type`;
+        tooltip.textContent = `Filter events by ${eventType} type. Read more here https://newtestamenttimeline.com/manuscript-types.html`;
         questionMark.appendChild(tooltip);
         legendItem.appendChild(checkbox);
         legendItem.appendChild(legendColor);
